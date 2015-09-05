f991c7fb342d559d14d3d6eea1ef8c66a1aaa8fb
toanhtran <toanh_tran@yahoo.com>
2015-09-06 04:53:41 +0900

    Add ru API joke link

--- css/quote.js
+++ css/quote.js
@@ -1,6 +1,15 @@
 $( document ).ready(function() {
-  function myFunction(){
-    var quotes =  
+  function 
+function myQuote() {
+  var placeholderDiv = document.getElementById(".jumbtron");
+  var url = "http://api.forismatic.com/api/1.0/";
+  var options = {
+    width: placeholderDiv.offsetWidth,
+    height: placeholderDiv.offsetHeight,
+    hideTabs: true,
+    hideToolbar: true,
+    
   };
-   
+  viz = new tableau.Viz(placeholderDiv, url, options);
+}  
 });
