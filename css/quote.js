d9e709e13c1e767a743f2d9d4fb8a0930d73bf72
toanhtran <toanh_tran@yahoo.com>
2015-09-07 10:12:15 +0900

    Add Star War questions array

--- css/quote.js
+++ css/quote.js
@@ -1,15 +1,47 @@
 $( document ).ready(function() {
-  function 
-function myQuote() {
-  var placeholderDiv = document.getElementById(".jumbtron");
-  var url = "http://api.forismatic.com/api/1.0/";
-  var options = {
-    width: placeholderDiv.offsetWidth,
-    height: placeholderDiv.offsetHeight,
-    hideTabs: true,
-    hideToolbar: true,
-    
-  };
-  viz = new tableau.Viz(placeholderDiv, url, options);
-}  
-});
+    var quotes = [{
+    	quote: "Judge me by my size, do you?",
+    	name:"Yoda"
+    }, {
+    	quote:"Do. Or do not. There is no try.",
+    	name: "Yoda" 
+    }, {
+    	quote:"Fear is the path to the dark side.",
+    	name:"Yoda"
+    }, {
+    	quote: "Truly wonderful, the mind of a child is.",
+    	name:"Yoda"
+    }, {
+    	quote:"A Jedi uses the Force for knowledge and defense, never for attack.",
+    	name: "Yoda"
+    }, {
+    	quote:"Adventure. Excitement. A Jedi craves not these things.",
+    	name:"Yoda"
+    }, {
+    	quote:"Fear is the path to the dark side…fear leads to anger…anger leads to hate…hate leads to suffering.",
+    	name: "Yoda"
+    }, {
+    	quote:"Use the Force, Luke.",
+    	name:"Obi-Wan Kenobi"
+    }, {
+    	quote:"In my experience there is no such thing as luck.",
+    	name:"Obi-Wan Kenobi"
+
+    },{
+    	quote:"Your focus determins your reality.",
+    	name: "Qui-Gon Jinn"
+    }, {
+    	quote:"Never tell me the odds",
+    	name:"Han Solo"
+    }, {
+    	quote: "Great,kid. Don't get cocky.",
+    	name:"Han Solo"
+    }, {
+    	quote: "You like me because I'm a scoundrel. There aren't enough scoundrels in your life.",
+    	name:"Han Solo"
+    }, {
+    	quote:"So? What are you going to do, talk me to death?",
+    	name:"Han Solo"
+    }
+
+    }]
