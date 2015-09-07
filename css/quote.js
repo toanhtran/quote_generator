33d9f1d66faa9931060bcafdf1b2050246ccd33a
toanhtran <toanh_tran@yahoo.com>
2015-09-07 10:53:41 +0900

    Add Stalinist One|Sigmar font

--- css/quote.js
+++ css/quote.js
@@ -14,10 +14,7 @@ $( document ).ready(function() {
     }, {
     	quote:"A Jedi uses the Force for knowledge and defense, never for attack.",
     	name: "Yoda"
-    }, {
-    	quote:"Adventure. Excitement. A Jedi craves not these things.",
-    	name:"Yoda"
-    }, {
+    },  {
     	quote:"Fear is the path to the dark side…fear leads to anger…anger leads to hate…hate leads to suffering.",
     	name: "Yoda"
     }, {
@@ -27,9 +24,6 @@ $( document ).ready(function() {
     	quote:"In my experience there is no such thing as luck.",
     	name:"Obi-Wan Kenobi"
 
-    },{
-    	quote:"Your focus determins your reality.",
-    	name: "Qui-Gon Jinn"
     }, {
     	quote:"Never tell me the odds",
     	name:"Han Solo"
@@ -42,6 +36,6 @@ $( document ).ready(function() {
     }, {
     	quote:"So? What are you going to do, talk me to death?",
     	name:"Han Solo"
-    }
+    }, 
 
     }]
